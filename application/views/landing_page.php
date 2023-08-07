f270cb8ad122f92ed708f473b488e3672fb95aa4
ariespirnando <aries.pirnando@gmail.com>
2023-08-07 20:57:18 +0700

    update design

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>World Vision</title>
+    <title>CRM V1</title>
     <!-- plugin css for this page -->
     <link
       rel="stylesheet"
@@ -21,7 +21,7 @@
       href="<?php echo base_url().'assets/landing_page/' ?>assets/vendors/owl.carousel/dist/assets/owl.theme.default.min.css"
     />
     <!-- End plugin css for this page -->
-    <link rel="shortcut icon" href="<?php echo base_url().'assets/landing_page/' ?>assets/images/favicon.png" />
+    <link rel="icon" type="image/x-icon" href="<?php echo base_url().'assets/' ?>assets/img/favicon.ico"/>
     <!-- inject:css -->
     <link rel="stylesheet" href="<?php echo base_url().'assets/landing_page/' ?>assets/css/style.css">
     <!-- endinject -->
